7100abf2eb4ecc7380c18860137445fe71d77e35
Valentyn Sydorenko <167340971+VSidd976@users.noreply.github.com>
2025-04-24 22:43:11 +0300

    Add files via upload

---
+++ get_hourly_telegram_data/hourly_scraper.py
@@ -0,0 +1,96 @@
+import os
+import json
+import csv
+import asyncio
+from telethon import TelegramClient
+from telethon.tl.types import User, PeerChannel
+from telethon.errors import FloodWaitError, RPCError
+import aiohttp
+import sys
+import pandas as pd
+
+
+STATE_FILE = 'state.json'
+
+
+def save_state(state):
+    with open(STATE_FILE, 'w') as f:
+        json.dump(state, f)
+
+
+def load_state():
+    if os.path.exists(STATE_FILE):
+        with open(STATE_FILE, 'r') as f:
+            return json.load(f)
+    return {
+        'api_id': None,
+        'api_hash': None,
+        'phone': None,
+        'channels': {},
+        'scrape_media': True,
+    }
+
+
+state = load_state()
+client = TelegramClient('session', state['api_id'], state['api_hash'])
+
+
+async def scrape_channel(client, channel, offset_id, df, state):
+    try:
+        if channel.startswith('-'):
+            entity = await client.get_entity(PeerChannel(int(channel)))
+        else:
+            entity = await client.get_entity(channel)
+
+        total_messages = 0
+        processed_messages = 0
+
+        async for message in client.iter_messages(entity, offset_id=offset_id, reverse=True):
+            total_messages += 1
+
+        if total_messages == 0:
+            print(f"No messages found in channel {channel}.")
+            return df
+
+        last_message_id = None
+        processed_messages = 0
+
+        async for message in client.iter_messages(entity, offset_id=offset_id, reverse=True):
+            try:
+                new_row = pd.DataFrame({'date': [message.date.strftime('%Y-%m-%d %H:%M:%S')], 'message': [message.text if message.text else ""]})
+
+                df = pd.concat([df, new_row], ignore_index=True)
+
+                last_message_id = message.id
+                processed_messages += 1
+
+                progress = (processed_messages / total_messages) * 100
+                sys.stdout.write(f"\rScraping channel: {channel} - Progress: {progress:.2f}%")
+                sys.stdout.flush()
+
+                state['channels'][channel] = last_message_id
+                save_state(state)
+            except Exception as e:
+                print(f"Error processing message {message.id}: {e}")
+        print()
+    except ValueError as e:
+        print(f"Error with channel {channel}: {e}")
+
+    return df
+
+
+async def main():
+    try:
+        await client.start()
+        df = pd.DataFrame(columns=['date', 'message'])
+        print("Hourly scraping began")
+        for channel in state['channels']:
+            df = await scrape_channel(client,channel, state['channels'][channel], df, state)
+        df.to_csv('daily_telegram_data.csv', index=False)
+    finally:
+        await client.disconnect()
+        print("Hourly scraping ended")
+
+
+if __name__ == '__main__':
+    asyncio.run(main())
